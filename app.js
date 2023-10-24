58669cb4c007bf9ec055441079235dda1bf438e3
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2023-10-24 16:09:12 +0100

    Estrutura inicial --- OK

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+require('dotenv').config();
+
+//Rotas aqui
+
+//const rotaLogin = require('./routes/login')
+
+
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header("Access-Control-Allow-Credentials", "true")
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS')
+        return res.status(200).send({})
+    }
+    next();
+})
+
+//EndPoints Aqui
+app.use('/login', rotaLogin);
+
+
+
+app.get('/api/test', (req,res) => {
+    res.status(200).json({message: 'OK'})
+})
+
+app.use(express.static('public'))
+
+app.use((req, res, next) => {
+    const erro = new Error('Rota não encontrada');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.send({
+        erro: {
+            mensagem: error.mensagem
+        }
+    })
+});
+
+module.exports = app;
---
+++ helpers/img-uploader.js
@@ -0,0 +1,32 @@
+const multer = require("multer");
+const path = require("path");
+
+// Destination to store image
+const imageStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let folder = "avatar";
+
+    if (req.baseUrl.includes('user')) {
+      folder = "avatar";
+    } else if (req.baseUrl.includes('avatar')) {
+      folder = "avatar";
+    }
+    cb(null, `public/${folder}/`);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg|pdf|jpeg)$/)) {
+      // upload only png and jpg format
+      return cb(new Error("Por favor, envie apenas png ou jpg!"));
+    }
+    cb(undefined, true);
+  },
+});
+
+module.exports = { imageUpload };
